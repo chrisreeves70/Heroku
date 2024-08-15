4cbdcdedf9b6441942579d8718c04690aa17577c
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-14 18:59:55 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-// Database URL from Heroku
+
 $databaseUrl = "postgres://untvvdv7d0ff:pf596cd903192c82834e2091c82761d383a55731b171fb4a308aeb014330a585b@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7fnbkag5p2q0g";
 
-// Parse the URL to extract connection information
+// URL to extract connection information
 $components = parse_url($databaseUrl);
 
 // Extract the connection details
