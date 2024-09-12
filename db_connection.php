b3824fad7cf941fc159ffdc9fbcd9c54b98371d4
chrisreeves70 <chrisreeves70@icloud.com>
2024-09-12 14:46:55 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$databaseUrl = "postgres://untvvdv7d0ff:pf596cd903192c82834e2091c82761d383a55731b171fb4a308aeb014330a585b@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7fnbkag5p2q0g";
+$databaseUrl = "mysql://bf1b99b2168a1d:3cc7e973@us-cluster-east-01.k8s.cleardb.net/heroku_8e2d5898e64e59e?reconnect=true";
 
 // URL to extract connection information
 $components = parse_url($databaseUrl);
