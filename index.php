c388ad01b3c7a145845a68c6cf85ffb3ed67161a
chrisreeves70 <chrisreeves70@icloud.com>
2024-09-12 18:17:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+// Include Composer's autoload file
+require 'vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\LogglyHandler;
+
+// Create a log channel
+$log = new Logger('my_app');
+$log->pushHandler(new LogglyHandler('2c8f1f6e-b7d3-4682-9b36-8b90ab233298', Logger::DEBUG));
+
+// Add a log entry
+$log->info('Index page accessed');
+
+// HTML content starts here
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
