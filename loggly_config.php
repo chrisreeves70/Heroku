fb6e87f464ed2a37169a94ae025f5c2796a51f75
chrisreeves70 <chrisreeves70@icloud.com>
2024-09-14 17:04:24 -0700

    Update loggly_config.php

--- loggly_config.php
+++ loggly_config.php
@@ -1,13 +1,18 @@
 <?php
+require 'vendor/autoload.php'; // Ensure you include Composer's autoload file
+
 use Monolog\Logger;
 use Monolog\Handler\LogglyHandler;
 
 // Initialize logger
 $log = new Logger('loggly');
 
-// Use your API Token and set the log level (e.g., INFO)
-$log->pushHandler(new LogglyHandler('2c8f1f6e-b7d3-4682-9b36-8b90ab233298', Logger::INFO));
+// Use your Loggly HTTPS endpoint URL including your API token
+$logglyToken = '2c8f1f6e-b7d3-4682-9b36-8b90ab233298'; // Replace this with your actual token
+$logglyUrl = "http://logs-01.loggly.com/inputs/9e1a9ba6-793d-4d1b-81d8-6cccf3511ba9/tag/http/
+";
+
+$log->pushHandler(new LogglyHandler($logglyUrl, Logger::INFO));
 
 // Example of logging a message
 $log->info('Log message to Loggly');
-
