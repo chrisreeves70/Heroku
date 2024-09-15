1bcbacc399e7950e5475b94fce8d7772edcc0543
chrisreeves70 <chrisreeves70@icloud.com>
2024-09-14 17:08:02 -0700

    Update loggly_config.php

--- loggly_config.php
+++ loggly_config.php
@@ -9,10 +9,14 @@ $log = new Logger('loggly');
 
 // Use your Loggly HTTPS endpoint URL including your API token
 $logglyToken = '2c8f1f6e-b7d3-4682-9b36-8b90ab233298'; // Replace this with your actual token
-$logglyUrl = "http://logs-01.loggly.com/inputs/9e1a9ba6-793d-4d1b-81d8-6cccf3511ba9/tag/http/
-";
+$logglyUrl = "https://logs-01.loggly.com/inputs/$logglyToken/tag/http/";
 
+// Push handler to Loggly
 $log->pushHandler(new LogglyHandler($logglyUrl, Logger::INFO));
 
 // Example of logging a message
-$log->info('Log message to Loggly');
+try {
+    $log->info('Log message to Loggly');
+} catch (Exception $e) {
+    echo 'Logging failed: ',  $e->getMessage(), "\n";
+}
